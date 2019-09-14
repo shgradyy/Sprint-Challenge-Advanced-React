b50c40a15978c70be69dc6d58cfc079502432871
Shantay Grady <shantaygrady@gmail.com>
2019-09-13 23:11:22 -0400

    Builds ButtonPanel component

---
+++ client/src/components/ButtonPanel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ButtonPanel = props => {
+    return (
+        <div className="buttonPanel">
+            <button></button>
+        </div>
+    )
+};
+export default ButtonPanel;
