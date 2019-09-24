ed2ef1bc000db148fcbbe3895a73515bb2815c82
Shantay Grady <shantaygrady@gmail.com>
2019-09-24 19:34:38 -0400

    adds api, imports dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,43 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ButtonPanel from './components/ButtonPanel';
 import DataDisplay from './components/DataDisplay';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My App</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    } 
+  }
+
+  componentDidMount() {
+    axios('http://localhost:5000/api/players')
+      .then(res => {
+        console.log(res.data)
+        this.setState({ players: res.data })
+      })
+      .catch(err => console.error('You have an error', err))      
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <Navbar />
+        {this.state.players.map( player => {
+          return (
+            <div className="players" key={player.id}>
+              <h3>{player.name}</h3>
+              <p>Country: {player.country}</p>
+              <p>Searches: {player.searches}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
+
 
 export default App;
