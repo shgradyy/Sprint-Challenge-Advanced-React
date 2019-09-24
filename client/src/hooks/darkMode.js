bd45e9a8b8bdec41e418779441f1fd0341def1c8
Shantay Grady <shantaygrady@gmail.com>
2019-09-24 19:49:07 -0400

    Creates custom hooks

---
+++ client/src/hooks/darkMode.js
@@ -0,0 +1,13 @@
+import React, { useEffect }from 'react';
+import { useLocalStorage } from './useLocalStorage.js'
+
+export const useDarkMode = () => {
+  const [storedValue, setValue ] = useLocalStorage('darkMode')
+
+  useEffect(() => {
+    let body = document.querySelector('body');
+    storedValue ? body.classList.add('dark-mode') : body.classList.remove('dark-mode');
+  }, [storedValue]);
+
+  return [storedValue, setValue]
+}
