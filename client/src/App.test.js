da6c09e5827177f34eb8fdfe8d9c7d45ce659d9e
Shantay Grady <shantaygrady@gmail.com>
2019-09-24 20:34:59 -0400

    Install testing dependencies, updates package json

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NavBar from './components/NavBar';
+import { render } from "@testing-library/react";
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
 });
+
+test('renders with no errors', () => {
+  const div = document.createElement('div')
+  render(<NavBar />, div);
+});
+
+test('displays title correctly', () => {
+  const {getByText} = render(<App />);
+  getByText("Players")
+})
